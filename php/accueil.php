4c4cc610c68eacf71196b181dabc1f39af974cc9
KOUADIO Josue <kouadiojosuenoelyao@gmail.com>
2024-10-25 00:26:13 +0000

    nav bar ok

--- php/accueil.php
+++ php/accueil.php
@@ -275,11 +275,14 @@
                 <span class="logo-mark">Mark</span><span class="logo-gros">gros</span>
             </a>
             <div class="user-actions">
-                <button class="icon-button">
-                    <svg width="24" height="24" viewBox="0 0 24 24">
-                        <path fill="currentColor" d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4Z"/>
-                    </svg>
-                </button>
+                <a href="infoPerson.php">
+                    <button class="icon-button">
+                        <svg width="24" height="24" viewBox="0 0 24 24">
+                            <path fill="currentColor" d="M12 4a4 4 0 0 1 4 4a4 4 0 0 1-4 4a4 4 0 0 1-4-4a4 4 0 0 1 4-4m0 10c4.42 0 8 1.79 8 4v2H4v-2c0-2.21 3.58-4 8-4Z"/>
+                        </svg>
+                    </button>
+                </a>
+
                 <button class="icon-button">
                     <svg width="24" height="24" viewBox="0 0 24 24">
                         <path fill="currentColor" d="M4 4h4v4H4V4m6 0h4v4h-4V4m6 0h4v4h-4V4M4 10h4v4H4v-4m6 0h4v4h-4v-4m6 0h4v4h-4v-4M4 16h4v4H4v-4m6 0h4v4h-4v-4m6 0h4v4h-4v-4"/>
